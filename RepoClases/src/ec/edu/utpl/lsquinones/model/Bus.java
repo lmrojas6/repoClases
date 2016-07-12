687ab05e35734f2d8a6fd3261a695a813f4b1d49
lmrojas6 <lmrojas6@users.noreply.github.com>
2016-07-12 11:02:45 -0500

    Luchito

--- RepoClases/src/ec/edu/utpl/lsquinones/model/Bus.java
+++ RepoClases/src/ec/edu/utpl/lsquinones/model/Bus.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package ec.edu.utpl.lsquinones.model;
 
@@ -6,6 +10,6 @@ package ec.edu.utpl.lsquinones.model;
  *
  * @author Santiago Quiñones
  */
-public class Bus {
+public class Luchito :v {
 
 }
